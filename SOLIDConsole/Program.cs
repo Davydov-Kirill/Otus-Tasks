b5af2fc2dfd2f012806ac0fee08d2a14534273ce
Кирилл Давыдов <d4vydovk@gmail.com>
2024-10-17 23:13:28 +0300

    Переименование решения. Добавление библиотеки.

--- SOLIDConsole/Program.cs
+++ SOLIDConsole/Program.cs
@@ -1,53 +1,46 @@
-﻿using System;
+﻿using GuessTheNumberLibrary;
+using GuessTheNumberLibrary.Interfaces;
 
 internal class Program
 {
-    /*
-    На примере реализации игры «Угадай число» продемонстрировать практическое применение SOLID принципов.
-    Программа рандомно генерирует число, пользователь должен угадать это число. 
-    При каждом вводе числа программа пишет больше или меньше отгадываемого. 
-    Кол-во попыток отгадывания и диапазон чисел должен задаваться из настроек.
-    В отчёте написать, что именно сделано по каждому принципу.
-    Приложить ссылку на проект и написать, сколько времени ушло на выполнение задачи.
-     */
-
-    private static void Main(string[] args)
+    private static void Main()
     {
-        Console.WriteLine("Добро пожаловать в игру \"Угадай число\"");
-        int attempts = 5;
-        int startRange = 0;
-        int endRange = 10;
-        Console.WriteLine($"У вас будет {attempts} попыток угадать число в диапозоне от {startRange} до {endRange}");
-        while (true)
+        IInputService inputService = new ConsoleInputService();
+        IOutputService outputService = new ConsoleOutputService();
+        IGameSettings gameSettings = new GameSettings(5, 0, 10);
+        IRandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+        Game game = new Game(inputService, outputService, randomNumberGenerator, gameSettings);
+
+        outputService.HelloMessage();
+        gameSettings.ShowSettings();
+
+        bool isExit = default;
+
+        while (!isExit)
         {
-            Console.WriteLine("Нажмите \"Enter\" чтобы начать");
+            Console.WriteLine("\nНажмите \"Enter\" чтобы начать или \"Esc\" чтобы выйти.");
             Console.WriteLine("Если вы хотите изменить настройки игры нажмите \"S\"");
-            ConsoleKeyInfo keyInfo = Console.ReadKey(true); 
-            // true значит, что нажатая клавиша не будет выводиться в консоль
 
-            if (keyInfo.Key == ConsoleKey.Enter)
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+            switch (keyInfo.Key)
             {
-                Console.WriteLine("Начинаем игру!");
-                // Запускаем игру
-                break;
+                case ConsoleKey.Enter:
+                    game.Start();
+                    break;
+                case ConsoleKey.Escape:
+                    Console.WriteLine("Хорошего дня! Возвращайтесь снова!");
+                    isExit = true;
+                    break;
+                case ConsoleKey.S:
+                    gameSettings.ChangeSettings();
+                    continue;
+                default:
+                    Console.WriteLine("Некорректный ввод. Попробуйте еще раз.");
+                    break;
             }
-            else if (keyInfo.Key == ConsoleKey.S)
-            {
-                Console.WriteLine("Изменение настроек игры...");
-                Console.Write("Введите количество попыток: ");
-                int.TryParse(Console.ReadLine(), out attempts);
-                Console.Write("Введите начальный диапозон: ");
-                int.TryParse(Console.ReadLine(), out startRange);
-                Console.Write("Введите конечный диапозон: ");
-                int.TryParse(Console.ReadLine(), out endRange);
-                Console.WriteLine($"У вас будет {attempts} попыток угадать число в диапозоне от {startRange} до {endRange}");
-                continue;
-            }
-            else Console.WriteLine("Некорректный ввод. Попробуйте еще раз.");
         }
-
-
-
 
     }
 }
